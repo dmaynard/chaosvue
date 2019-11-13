e6ebe055cbaf9ffee26187aba9f6e92ae052d51a
dmaynard <davidsmaynard@gmail.com>
2019-11-12 20:54:15 -0800

    Added Continue
    refactoring for animationFrame

--- src/components/ChaosCanvas.vue
+++ src/components/ChaosCanvas.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="chaos-canvas-wrapper">
   <canvas ref="chaos-canvas" @click="clickMethod"></canvas>
-  <button v-on:click="start">Generate Attractor</button>
-  <button v-on:click="timeTest">Test Attractor</button>
+  <button v-on:click="startAttractor">Start Attractor</button>
+  <button v-on:click="continueAttractor">Continue</button>
+
+  <div v-if=darkmode>
+    <button v-on:click="toggleLightMode">Light Mode</button>
+  </div>
+  <div v-else>
+    <button v-on:click="toggleLightMode">Dark Mode</button>
+  </div>
   <slot></slot>
 </div>
 </template>
@@ -23,7 +30,14 @@ export default {
       a: 0.1,
       b: 0.2,
       c: 0.3,
-      d: 0.4
+      d: 0.4,
+      darkmode: false,
+      doPixel: null,
+      iters: 0,
+      itersPerFrame: 10000,
+      nTouched: 0,
+      nMaxed: 0,
+      state: "paused"
 
     }
   },
@@ -41,10 +55,22 @@ export default {
     this.ctx.drawImage(this.image, 0, 0, this.width, this.height);
     this.imageData = this.ctx.getImageData(0, 0, this.width, this.height);
     this.data = this.imageData.data;
+    this.doPixel = this.darkmode? this.incPixel : this.decPixel;
 
     // canvasImg.src = "/DSMLego350.jpg";
   },
   methods: {
+    initAttractor () {
+      this.iters = 0;
+      this.itersPerFrame = 100000,
+      this.nTouched = 0;
+      this.nMaxed = 0;
+      this.state = "running";
+      this.a = 3.0 * ((Math.random() * 2.0) - 1.0);
+      this.b = 3.0 * ((Math.random() * 2.0) - 1.0);
+      this.c = ((Math.random() * 2.0) - 1.0) + 0.5;
+      this.d = ((Math.random() * 2.0) - 1.0) + 0.5;
+    },
     timeIt(context, f, ...params) {
       let elapsed = -new Date().getTime();
       f.call(context, ...params);
@@ -52,18 +78,12 @@ export default {
       window.console.log(f.name + " : " + elapsed + " ms");
       return elapsed;
     },
-
     invert(r, g, b) {
       for (var i = 0; i < this.data.length; i += 4) {
         this.data[i] ^= r; // red
         this.data[i + 1] ^= g; // green
         this.data[i + 2] ^= b; // blue
-
       }
-
-    },
-    start() {
-      this.timeIt(this, this.generate);
     },
     zeroImage() {
       for (var i = 0; i < this.data.length; i += 4) {
@@ -73,22 +93,13 @@ export default {
         this.data[i + 3] = 255; // opaque
       }
     },
-    fillImage( r, g, b) {
+    fillImage(r, g, b) {
       for (var i = 0; i < this.data.length; i += 4) {
         this.data[i] = r; // red
         this.data[i + 1] = g; // green
         this.data[i + 2] = b; // blue
         this.data[i + 3] = 255; // opaque
       }
-    },
-    generate() {
-      for (var i = 0; i < this.data.length; i += 4) {
-        this.data[i] = Math.random() * 255; // red
-        this.data[i + 1] = Math.random() * 255; // green
-        this.data[i + 2] = Math.random() * 255; // blue
-        this.data[i + 3] = 255; // opaque
-      }
-      this.ctx.putImageData(this.imageData, 0, 0)
     },
     clickMethod() {
       this.timeIt(this, this.invert, 0xFF, 0xFF, 0xFF);
@@ -98,43 +109,44 @@ export default {
       this.zeroImage();
       this.timeIt(this, this.testAttractor);
     },
-    testAttractor() {
+    startAttractor() {
+       this.iterateAttractor(true);
+    },
+    continueAttractor() {
+       this.iterateAttractor(false);
+    },
+    iterateAttractor(init) {
+      let elapsed = - new Date().getTime();
       let px = 0;
       let py = 0;
-      let x = 0.1;
-      let nx;
-      let y = (Math.random() - 0.5);
-      // let z = (Math.random() - 0.5);
-      let ny = .1;
 
-      this.a = 3.0 * ((Math.random() * 2.0) - 1.0);
-      this.b = 3.0 * ((Math.random() * 2.0) - 1.0);
-      this.c = ((Math.random() * 2.0) - 1.0) + 0.5;
-      this.d = ((Math.random() * 2.0) - 1.0) + 0.5;
-      /*
-      this.a =  1.0;
-      this.b =  1.8;
-      this.c = 0.71;
-      this.d = 1.51;
-      */
+      let x = 0.1;
+      let y = 0.1;
+      let nx = 0;
+      let ny = 0;
       let xmin = 100;
-      let ymin = 100;
       let xmax = -100;
+      let ymin = 100;
       let ymax = -100;
+
+      if (init ) {
+      this.initAttractor();
       this.ctx.clearRect(0, 0, this.width, this.height);
-      this.zeroImage(),
-        this.ctx.putImageData(this.imageData, 0, 0)
-      for (var i = 0; i < 1000000; i++) {
+      if (this.darkmode) {
+        this.zeroImage();
+      } else {
+        this.fillImage(0xFF, 0xFF, 0xFF);
+      }
+      this.ctx.putImageData(this.imageData, 0, 0);
+      }
+      for (var i = 0; i < this.itersPerFrame; i++) {
         /* eslint-disable no-console */
         // console.log (" x " + x + " y " + y);
+        this.iters++;
 
         nx = Math.sin(y * this.b) - (this.c * Math.sin(x * this.b));
         ny = Math.sin(x * this.a) + this.d * Math.cos(y * this.a);
 
-        /*
-        nx = Math.sin(y*this.b) + (this.c * Math.sin(x*this.b));
-        ny = ((Math.sin(x*this.a))) + (this.d *  Math.sin(y*this.a));
-        */
         x = nx;
         y = ny;
         if (x < xmin) xmin = x;
@@ -143,10 +155,16 @@ export default {
         if (y > ymax) ymax = y;
         px = this.pixelx(x);
         py = this.pixely(y);
-        this.incPixel(px, py);
+        this.doPixel(px, py);
+        // this.testPixel(px,py);
       }
-      console.log(" xmin " + xmin + " xmax " + xmax + " ymin " + ymin + " ymax " + ymax);
-      this.ctx.putImageData(this.imageData, 0, 0)
+
+      console.log(" nTouched: " + this.nTouched + " nMaxed " + this.nMaxed + " this.iters " + this.iters + " % touched " +
+       (this.nTouched * 100.0 / (this.width * this.height)));
+      this.ctx.putImageData(this.imageData, 0, 0);
+      elapsed += new Date().getTime();
+      console.log(" elapsed: " + elapsed + " ms");
+      this.state = "paused";
     },
     pixelx(x) {
       let px = Math.floor(x * this.width / 6) + this.width / 2;
@@ -160,17 +178,52 @@ export default {
     },
     incPixel(x, y) {
       let i = (y * this.width + x) * 4;
+      if (this.data[i] == 0) this.nTouched++;
+      if (this.data[i] == 254) this.nMaxed++;
       if (this.data[i] < 255) {
         this.data[i] += 1;
         this.data[i + 1] += 1;
         this.data[i + 2] += 1;
-
       }
-
+      this.data[i + 3] = 255;
+    },
+    decPixel(x, y) {
+      let i = (y * this.width + x) * 4;
+      if (this.data[i] == 255) this.nTouched++;
+      if (this.data[i] == 1) this.nMaxed++;
+      if (this.data[i] > 0) {
+        this.data[i] += -1;
+        this.data[i + 1] += -1;
+        this.data[i + 2] += -1;
+      }
+      if (this.data[i] !== this.data[i + 1]) {
+        console.log(" not grayscale " + this.data[i] + this.data[i + 1]);
+      }
       this.data[i + 3] = 255;
 
+    },
+    testPixel(x, y) {
+      let i = (y * this.width + x) * 4;
+      if (this.data[i] !== this.data[i + 1]) {
+        console.log(" not grayscale " + this.data[i] + this.data[i + 1]);
+      }
+      if (this.data[i+1] !== this.data[i + 2]) {
+        console.log(" not grayscale " + this.data[i] + this.data[i + 1]);
+      }
+    },
+    toggleLightMode() {
+      this.darkmode = !this.darkmode;
+      if (this.darkmode) {
+        this.invert(0xFF, 0xFF, 0xFF);
+        this.doPixel = this.incPixel;
+      } else {
+        this.invert(0xFF, 0xFF, 0xFF);
+        this.doPixel = this.decPixel;
+      }
+      this.ctx.putImageData(this.imageData, 0, 0);
     }
   },
+
 
 }
 </script>
